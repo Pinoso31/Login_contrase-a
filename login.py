693348dbff0f04736195db0c0b82f3124ab0c431
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-06-26 08:49:52 -0600

    alteracion de lineas

--- login.py
+++ login.py
@@ -5,31 +5,39 @@ import getpass
 import os
 import pwinput
 
+def continuar():
+  input("Enter para continuar") 
+
 def cargar_usuario():
-    usuario = {}
-    if os.path.exits("usuarios.txt"):
-        with open("usuarios.txt", "r")as archivo:
+    usuarios = {} 
+    if os.path.exists("usuarios.txt"): 
+        with open("usuarios.txt", "r") as archivo:
             for linea in archivo:
-                usuario, clave = linea.strip().split(" , ")
+                usuario, clave = linea.strip().split(", ") 
                 usuarios[usuario] = clave
     return usuarios
 
 def agregar_usuario(usuario, clave):
-    with open("usuario.txt", "a")as archivo:
-              archivo.write(f"{usario}, {clave}")
-
-
+    with open("usuarios.txt", "a") as archivo: 
+        archivo.write(f"{usuario}, {clave}\n")  
 
 def inicio():
-    print("INICIO DE SESION ")
+    print("INICIO DE SESION")
     usuarios = cargar_usuario()
-    usuario = input("Usuario : ")
+    usuario = input("Usuario: ")
     
-    clave_ingresada = pwinput.pwinput(prompt = "Contraseña : ", mask = " * ")
+    clave_ingresada = pwinput.pwinput(prompt="Contraseña: ", mask="*")  
     if usuario in usuarios and usuarios[usuario] == clave_ingresada:
         print("Acceso permitido")
         return True
     else: 
-        print(" Usuario o contrañeña incorrecta.")
+        print("Usuario o contraseña incorrecta.")  
+        continuar()
+        os.system("cls")
+        
         return False
-inicio()
+def main():
+    inicio()
+ 
+
+main()
